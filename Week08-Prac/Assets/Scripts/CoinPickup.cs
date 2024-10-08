a61287d68fd8900cf7986e3024795ab04e70930d
Werdna6001 <109929421+Werdna6001@users.noreply.github.com>
2024-10-08 12:12:05 +1100

    Made the coin script 3D

--- Week08-Prac/Assets/Scripts/CoinPickup.cs
+++ Week08-Prac/Assets/Scripts/CoinPickup.cs
@@ -4,29 +4,30 @@ using System.Collections;
 public class CoinPickup : MonoBehaviour {
 
 	[SerializeField] private LayerMask playerLayer;
-	private AudioSource sound;
+	// private AudioSource sound;
 	private bool isGone = false;
 	public bool destroy = false;
+	public int pointsValue = 5;
 
 	public void Start() {
-		sound = gameObject.GetComponent<AudioSource>();
+		// sound = gameObject.GetComponent<AudioSource>();
 	}
 
-	public void OnTriggerEnter2D(Collider2D collider) {
+	public void OnTriggerEnter(Collider collider) {
 		if (isGone){
 			return;
 		}
 		if (((1 << collider.gameObject.layer) & playerLayer.value) != 0) {
-			if (sound != null) {
-				sound.Play();
-			}
-			SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+			// if (sound != null) {
+			// 	sound.Play();
+			// }
+			MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
 			renderer.enabled = false;
 			isGone = true;
 		}
 	}
 
-	public void OnTriggerExit2D(Collider2D collider) {
+	public void OnTriggerExit(Collider collider) {
 		if (((1 << collider.gameObject.layer) & playerLayer.value) != 0) {
 			if(destroy) {
 				Destroy(gameObject);
