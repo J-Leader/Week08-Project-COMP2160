12022f7507ac1415e9f7ccce4636adf03d97ac66
Werdna6001 <109929421+Werdna6001@users.noreply.github.com>
2024-10-08 12:58:09 +1100

    Calling the event now, I think

--- Week08-Prac/Assets/Scripts/Player.cs
+++ Week08-Prac/Assets/Scripts/Player.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
+    static private Player instance;
+    static public Player Instance {
+        get {
+            if (instance == null) {
+                Debug.LogError("There is no Player instance in the scene.");
+            }
+            return instance;
+        }
+    }
+    public event OnPickupEvent PickupEvent;
+
     public int totalScore = 0;
-
-	public void Start() {
-
-	}
 
 	public void OnTriggerEnter(Collider collider) {
         int pointsValue = collider.gameObject.GetComponent<CoinPickup>().pointsValue;
         totalScore += pointsValue;
+        PickupEvent?.Invoke(pointsValue)
         //declare point scored
 	}
 }
